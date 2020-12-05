d92707c27b2bc6775b4f615278e5e29e26c8ce77
Donghan Park <donghan.park@duke.edu>
2020-12-05 11:22:03 -0600

    added components

---
+++ src/Components/ProfileHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+    return(
+        <div className='main'>
+            <h1 className='title'>[username]</h1>
+            <p>[desc]</p>
+        </div>
+    );
+}
+
+export default Main;
