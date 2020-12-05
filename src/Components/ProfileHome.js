04436f82fdbdad6f620a6a29e2187671922034ed
Donghan Park <donghan.park@duke.edu>
2020-12-05 16:36:22 -0600

    updated outline

--- src/Components/ProfileHome.js
+++ src/Components/ProfileHome.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import { Info } from '@material-ui/icons';
+import React, { useState} from 'react';
 import './ProfileHome.css';
 
-/*
-    Sidebar: 
-        username, profile pic
+// Info box component
+function InfoBox(props) {
+    return(
+        <div className='infobox'>
+            <p className='infobox-title'>{props.title}</p>
+            <p className='infobox-value'>{props.value}</p>
+        </div>
+    );
+}
 
-    Charities invested
-    Amount 
-*/
+function NavButton(props) {
+    return (
+        <div className='nav-button-container'>
+            <img className='nav-button-icon'/>
+            <button className='nav-button'>
+                {props.title}
+            </button>
+        </div>
+    );
+}
 
 export default class HomeScreen extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            user: 'andrewlee'
+            username: 'andrew.lee',
+            navbar: [
+                { title: '[button name]', link: '' },
+                { title: '[button name]', link: '' }
+            ],
+            infobox: [
+                { title: 'amount invested', value: '$50.00' },
+                { title: 'charities', value: '4' },
+                { title: 'monthly payment', value: '$20.00' },
+                { title: 'charities', value: '4' }
+            ]
         };
     }
 
@@ -22,16 +46,17 @@ export default class HomeScreen extends React.Component{
             <div className='Main'>
                 <div className='Background'></div>
                 <div className='Outer-Container'>
-                    <div className='Profile-Container'>
+                    <div className='sidebar'>
                         <img className='User-Image'/>
-                        <h1 className='Title'>{this.state.user}</h1>
-                        <p>[desc]</p>
+                        <h1 className='Title'>{this.state.username}</h1>
+                        {this.state.navbar.map(button => (
+                            <NavButton title={button.title}/>
+                        ))}
                     </div>
-                    <div className='Info-Container'>
-                        <h1>YOUR CHARITIES</h1>
-                        <div className='yes'>
-                            <h1>[...]</h1>
-                        </div>
+                    <div className='info-container'>
+                        {this.state.infobox.map(info => (
+                            <InfoBox title={info.title} value={info.value}/>
+                        ))}
                     </div>
                 </div>
             </div>
