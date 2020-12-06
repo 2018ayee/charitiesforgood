9faf04c9d296b7250de598a4b85060e4266df950
Karen Deng <karen.deng@duke.edu>
2020-12-05 23:57:23 -0500

    fixed v2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import firebase from 'firebase';
 import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import SetupScreen from './screens/SetupScreen';
-
 import SignUp from "./Components/SignUp.js"
 import Charities from './Components/Charities';
-import SetupScreen from './screens/SetupScreen.js'
 import Profile from './Components/Profile';
 
 function App() {
